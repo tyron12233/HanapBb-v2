69140649912db06d7b9e025269dd5cc943a16fe3
HanapBb <lucena.tyronscott@gmail.com>
2020-11-26 15:09:21 +0800

    Initial release

---
+++ app/src/main/java/com/tyron/hanapbb/MyApplication.java
@@ -0,0 +1,25 @@
+package com.tyron.hanapbb;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.tyron.hanapbb.emoji.EmojiManager;
+import com.tyron.hanapbb.emoji.ios.IosEmojiProvider;
+import com.tyron.hanapbb.messenger.AndroidUtilities;
+
+public class MyApplication extends Application {
+
+    public static Context applicationContext;
+    public static volatile Handler applicationHandler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EmojiManager.install(new IosEmojiProvider());
+        applicationContext = this;
+        AndroidUtilities.fillStatusBarHeight(applicationContext);
+        applicationHandler = new Handler(applicationContext.getMainLooper());
+    }
+}
